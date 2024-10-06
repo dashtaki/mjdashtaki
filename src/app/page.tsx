2dc59c3a1810470ccd04388652f87ac1b778fccb
Mohammad Jamal Dashtaki <m.dashtaki@epilot.cloud>
2024-10-06 11:57:26 +0200

    Add Prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,59 +1,82 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import { Github, Linkedin, FileText, Calendar } from 'lucide-react'
+import Image from 'next/image';
+import Link from 'next/link';
+import { Github, Linkedin, FileText, Calendar } from 'lucide-react';
 
 export default function MainPage() {
   return (
-    <div className="min-h-screen bg-gray-100 py-12 px-4 sm:px-6 lg:px-8 flex justify-center items-center max-sm:py-4">
-      <div className="max-w-3xl mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-4xl">
-        <div className="md:flex">
-          <div className="md:shrink-0">
+    <div className='flex min-h-screen items-center justify-center bg-gray-100 px-4 py-12 max-sm:py-4 sm:px-6 lg:px-8'>
+      <div className='mx-auto max-w-3xl overflow-hidden rounded-xl bg-white shadow-md md:max-w-4xl'>
+        <div className='md:flex'>
+          <div className='md:shrink-0'>
             <Image
-              className="h-48 w-full object-cover md:h-32 md:w-25 max-sm:h-80"
-              src="/mj.jpg"
-              alt="Profile picture"
+              className='h-48 w-full object-cover max-sm:h-80 md:h-32 md:w-25'
+              src='/mj.jpg'
+              alt='Profile picture'
               width={400}
               height={400}
             />
           </div>
-          <div className="p-8">
-            <h1 className="block mt-1 text-4xl leading-tight font-bold text-black">MJ Dashtaki</h1>
-            <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold text-lg">Senior Frontend Engineer
+
+          <div className='p-8'>
+            <h1 className='mt-1 block text-4xl font-bold leading-tight text-black'>
+              MJ Dashtaki
+            </h1>
+
+            <div className='text-sm font-semibold uppercase tracking-wide text-indigo-500'>
+              Senior Frontend Engineer
             </div>
-            <p className="mt-6 text-gray-700 leading-7">
+
+            <p className='mt-6 leading-7 text-gray-700'>
               {`I'm the type who enjoys challenges that shove me (sometimes not-so-gently) and designing code mostly with Javascript 10+ years.
                 Over this time, I've worked with a variety of JavaScript frameworks and libraries, including Angular, React, and Svelte. From boosting
                 performance to improving user experience and investigate solutions by discovery and delivery, all is what I'm up for it!`}
-                </p>
-            <div className="mt-6 flex space-x-4">
+            </p>
+
+            <div className='mt-6 flex space-x-4'>
               <Link
-                href="https://github.com/dashtaki"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-gray-600 hover:text-gray-800 transition-colors"
-              > <Github className="h-6 w-6" /> <span className="sr-only">GitHub</span> </Link> <Link
-              href="https://linkedin.com/in/mjdashtaki"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-gray-600 hover:text-gray-800 transition-colors"
-            > <Linkedin className="h-6 w-6" /> <span className="sr-only">LinkedIn</span> </Link> <Link
-              href="/Mohammad_Jamal_Dashtaki_CV.pdf"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-gray-600 hover:text-gray-800 transition-colors"
-            > <FileText className="h-6 w-6" /> <span className="sr-only">CV</span> </Link>
+                href='https://github.com/dashtaki'
+                target='_blank'
+                rel='noopener noreferrer'
+                className='text-gray-600 transition-colors hover:text-gray-800'
+              >
+                <Github className='h-6 w-6' />
+                <span className='sr-only'>GitHub</span>
+              </Link>
+
+              <Link
+                href='https://linkedin.com/in/mjdashtaki'
+                target='_blank'
+                rel='noopener noreferrer'
+                className='text-gray-600 transition-colors hover:text-gray-800'
+              >
+                <Linkedin className='h-6 w-6' />
+                <span className='sr-only'>LinkedIn</span>
+              </Link>
+
+              <Link
+                href='/Mohammad_Jamal_Dashtaki_CV.pdf'
+                target='_blank'
+                rel='noopener noreferrer'
+                className='text-gray-600 transition-colors hover:text-gray-800'
+              >
+                <FileText className='h-6 w-6' />
+                <span className='sr-only'>CV</span>
+              </Link>
             </div>
-            <div className="mt-8">
+
+            <div className='mt-8'>
               <Link
-                href="https://adplist.org/mentors/mohammadjamal-dashtaki"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              > <Calendar className="h-5 w-5 mr-2" /> Plan a meeting </Link>
+                href='https://adplist.org/mentors/mohammadjamal-dashtaki'
+                target='_blank'
+                rel='noopener noreferrer'
+                className='inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
+              >
+                <Calendar className='mr-2 h-5 w-5' /> Plan a meeting
+              </Link>
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,27 +1,31 @@
 const config: {
   plugins: any[];
   theme: {
-    extend: { width: { '25': string }; colors: { background: string; foreground: string }; height: { '32': string } }
+    extend: {
+      width: { '25': string };
+      colors: { background: string; foreground: string };
+      height: { '32': string };
+    };
   };
-  content: string[]
+  content: string[];
 } = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
       },
       width: {
         '25': '25rem',
       },
       height: {
         '32': '32rem',
-      }
+      },
     },
   },
   plugins: [],
